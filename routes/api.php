b324e85a5747094b36f099da84e9f5d5fa6161b3
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2024-05-23 21:32:38 -0300

    API de Pets

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,17 @@ use App\Http\Controllers\Api\PetsApiController;
 
 Route::get('/pets', [PetsApiController::class, 'getList']);
 Route::post('/pets', [PetsApiController::class, 'add']);
+Route::get('/pets/{id}', [PetsApiController::class, 'getOne']);
+Route::patch('/pets/{id}', [PetsApiController::class, 'edit']);
+Route::delete('/pets/{id}', [PetsApiController::class, 'delete']);
+
+
+// Route::prefix('/pets')
+//     ->controller(PetsApiController::class)
+//     ->group(function () {
+//         Route::get('/', 'getList');
+//         Route::get('/{id}', 'getOne');
+//         Route::post('/', 'add');
+//         Route::patch('/{id}', 'edit');
+//         Route::delete('/{id}', 'delete');
+//     });
